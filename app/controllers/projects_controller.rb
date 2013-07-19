d0b890ee73a85f987f94daf21221d89a5cc9f010
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2013-07-19 17:15:41 +0200

    basespace configuration is coming from the web app container.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,15 +3,18 @@ class ProjectsController < ApplicationController
   def index
     @index = "Something relevant"
 
-    @bs = BaseSpaceAPI.start
+    # @bs = BaseSpaceAPI.start
+
+    bs=Rails.configuration.basespace
+
     @result_files = []
     @results = []
     # @xm = ::Builder::XmlMarkup.new(:indent => 2)
-    projects = @bs.get_project_by_user('current')
+    projects = bs.get_project_by_user('current')
     projects.each do |project|
-      @results << results = project.get_app_results(@bs)
+      @results << results = project.get_app_results(bs)
       results.each do |result|
-        result.get_files(@bs).each do |file|
+        result.get_files(bs).each do |file|
           @result_files << file
         end
       end
--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -3,15 +3,17 @@ class SamplesController < ApplicationController
   def index
     @index = "Something relevant"
 
-    @bs = BaseSpaceAPI.start
+    # @bs = BaseSpaceAPI.start
+    bs=Rails.configuration.basespace
+
     @sample_files = []
     @samples = []
     # @xm = ::Builder::XmlMarkup.new(:indent => 2)
-    projects = @bs.get_project_by_user('current')
+    projects = bs.get_project_by_user('current')
     projects.each do |project|
-      @samples << samples = project.get_samples(@bs)
+      @samples << samples = project.get_samples(bs)
       samples.each do |sample|
-        files = sample.get_files(@bs)
+        files = sample.get_files(bs)
         files.each do |file|
           @sample_files << file
         end
